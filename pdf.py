6644caf503cd73769a6e5847049d1265ac812ab4
Seif Gamal Al-Rahmany <seif924@hotmail.com>
2025-07-10 11:21:28 +0300

    pull

---
+++ pdf.py
@@ -0,0 +1,18 @@
+from fpdf import FPDF
+import os 
+
+# pdf = FPDF(orientation='P', unit='mm', format='A5')
+# pdf.add_page()
+# pdf.set_font("Arial", size=12)
+# pdf.cell(100, 10, txt="This is an A5-sized invoice", ln=True)
+# pdf.output("invoice.pdf")
+
+
+# pdf = FPDF(orientation='P', unit='mm', format='A4')
+# pdf.add_page()
+# pdf.set_font("Arial", size=14)
+# pdf.cell(100, 10, txt="This is an A5-sized invoice", ln=True)
+# pdf.output("invoiceA4.pdf")
+
+# os.startfile(r"C:\Users\seif9\Desktop\app\Hassan-Factory-App\invoiceA4.pdf", "print")
+os.startfile(r"C:\Users\seif9\Desktop\app\Hassan-Factory-App\فاتورة بيع_0517.pdf", "print")
